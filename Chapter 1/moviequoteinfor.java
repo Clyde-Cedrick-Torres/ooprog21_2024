ccd6537d746dc3050bd341da8ee004e880fcdf5b
Clyde-Cedrick-Torres <torresclyde13445@gmail.com>
2024-08-14 21:27:26 +0800

    Create moviequoteinfor.java

---
+++ Chapter 1/moviequoteinfor.java
@@ -0,0 +1,19 @@
+public class MovieQuoteInfo { 
+    
+   
+    public static void main(String[] args) {
+        printQuote(); 
+    }
+    
+
+    public static void printQuote() {
+        
+        System.out.println("Quote: \"I Giorno Giovanna Have A Dream.\"");
+        
+        System.out.println("Movie/Anime: JoJo's Bizarre Adventure");
+        
+        System.out.println("Character: Giorno Giovanna");
+        
+        System.out.println("Year: 2012 released");
+    } 
+}
