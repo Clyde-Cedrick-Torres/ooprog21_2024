414ab160641643c96c01438ae08c90a162e9cfb4
Clyde-Cedrick-Torres <torresclyde13445@gmail.com>
2024-09-20 19:50:44 +0800

    Create FirstAdressExcercise.java

---
+++ Chapter 3/FirstAdressExcercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExercise {
+
+   public static void main(String[] args) {
+            System.out.println("Google Pixel");
+
+             displayAddress();
+
+            System.out.println("Company: Google Pixel Inc.");
+      }
+
+     public static void displayAddress() {
+      System.out.println("Mountain View, California, United States");
+      System.out.println("1600 Amphitheatre Parkway Mountain View, CA 94043");
+    }
+}
